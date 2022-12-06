fc5ffe08fc8184ade66729cacbe17df5410d9f88
SKY-Tobi <tobias.heierli@lernende.bbw.ch>
2022-12-06 22:28:58 +0100

    Converted Day 5 Solution Code from Java to Kotlin

--- src/main/kotlin/dayFive/PartOne.kt
+++ src/main/kotlin/dayFive/PartOne.kt
@@ -1,122 +1,124 @@
-package dayFive;
+package dayFive
 
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
-import java.util.concurrent.atomic.AtomicBoolean;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
-import java.util.stream.IntStream;
+import java.util.*
+import java.util.regex.Pattern
+import kotlin.system.exitProcess
 
-public class PartOne {
-
-    private static final int SHIP_HEIGHT = 40;
-    private static final String EMPTY_CONTAINER = "    ";
-
-    public static void execute(List<String> lines) {
-        Ship ship = analyseShipSize(lines);
-        String[][] cargo = ship.getCargo();
-        AtomicBoolean hasFieldBeenInitialized = new AtomicBoolean(false);
-        IntStream.range(0, lines.size()).forEach(index -> {
-            String line = lines.get(index);
-            if (!hasFieldBeenInitialized.get()) {
-                try {
-                    // check if line only contains numbers
-                    Integer.parseInt(line.replaceAll("\\s", ""));
-                    hasFieldBeenInitialized.set(true);
-                } catch (Exception ignored) {
-                    Pattern p = Pattern.compile("\\s{4}|(\\[[A-Z]\\])");
-                    Matcher m = p.matcher(line);
-                    int i = 0;
-                    while (m.find()) {
-                        if (!m.group().trim().isEmpty())
-                            cargo[index + SHIP_HEIGHT - ship.getInitializeHighestPoint()][i] = m.group() + " ";
-                        i++;
-                    }
-                }
-            } else {
-                //get input (amount to move, fromColumn and toColumn) (3 Input parameters else skip)
-                List<String> splitInput = new ArrayList<>();
-                Pattern p = Pattern.compile("\\d+");
-                Matcher m = p.matcher(line);
-                while (m.find()) {
-                    splitInput.add(m.group());
-                }
-
-                if (splitInput.size() == 3) {
-                    int amount = Integer.parseInt(splitInput.get(0));
-                    int fromX = Integer.parseInt(splitInput.get(1)) - 1;
-                    int toX = Integer.parseInt(splitInput.get(2)) - 1;
-                    List<String> tempCrane = new ArrayList<>();
-                    int amountLeftToMove = amount;
-                    for (int y = 0; amountLeftToMove != 0 && y < SHIP_HEIGHT; y++) {
-                        if (!cargo[y][fromX].trim().isEmpty()) {
-                            tempCrane.add(cargo[y][fromX]);
-                            cargo[y][fromX] = EMPTY_CONTAINER;
-                            amountLeftToMove--;
-                        }
-                    }
-                    //find Index where container starts (when column is empty, then SHIP_HEIGHT + 1)
-                    int highestContainerIndex = SHIP_HEIGHT;
-                    for (int y = 0; highestContainerIndex == SHIP_HEIGHT && y < SHIP_HEIGHT; y++) {
-                        if (!cargo[y][toX].trim().isEmpty()) {
-                            highestContainerIndex = y;
-                        }
-                    }
+class PartOne {
+    companion object {
+        private const val SHIP_HEIGHT = 40
+        private const val EMPTY_CONTAINER = "    "
+        fun execute(lines: List<String>) {
+            val ship = analyseShipSize(lines)
+            val cargo = ship.cargo
+            var hasFieldBeenInitialized = false
+            lines.forEachIndexed { index, it ->
+                if (!hasFieldBeenInitialized) {
                     try {
-                        //start filling column to the top till no more container are left to move
-                        for (int y = highestContainerIndex - 1; y > highestContainerIndex - 1 - tempCrane.size(); y--) {
-                            cargo[y][toX] = tempCrane.get(highestContainerIndex - 1 - y);
+                        // check if line only contains numbers
+                        it.replace("\\s".toRegex(), "").toInt()
+                        hasFieldBeenInitialized = true
+                    } catch (ignored: Exception) {
+                        val p = Pattern.compile("\\s{4}|(\\[[A-Z]])")
+                        val m = p.matcher(it)
+                        var i = 0
+                        while (m.find()) {
+                            if (m.group().trim { it <= ' ' }.isNotEmpty()) {
+                                cargo[index + SHIP_HEIGHT - ship.initializeHighestPoint][i] = m.group() + " "
+                            }
+                            i++
                         }
-                    } catch (Exception e) {
-                        System.err.println("amount: " + amount + ", fromX: " + fromX + ", toX " + toX);
-                        System.exit(4);
                     }
                 } else {
-                    System.out.println("line skipped: " + index);
-                }
-            }
-        });
-        System.out.println("End State of Cargos: ");
-        for (String[] row : cargo) System.out.println(Arrays.toString(row));
-        System.out.println();
-        //Get Highest Container Letter
-        StringBuilder stringBuilder = new StringBuilder();
-        for (int x = 0; x < ship.getCargoColumns(); x++) {
-            for (int y = 0; y < SHIP_HEIGHT; y++) {
-                if (!cargo[y][x].trim().isEmpty()) {
-                    Pattern p = Pattern.compile("[A-Z]");
-                    Matcher m = p.matcher(cargo[y][x]);
-                    if (m.find()) {
-                        stringBuilder.append(m.group());
-                        break;
+                    //get input (amount to move, fromColumn and toColumn) (3 Input parameters else skip)
+                    val splitInput: MutableList<Int> = ArrayList()
+                    val p = Pattern.compile("\\d+")
+                    val m = p.matcher(it)
+                    while (m.find()) {
+                        splitInput.add(m.group().toInt())
+                    }
+                    if (splitInput.size == 3) {
+                        val amount = splitInput[0]
+                        val fromX = splitInput[1] - 1
+                        val toX = splitInput[2] - 1
+                        val tempCrane: MutableList<String?> = ArrayList()
+                        var amountLeftToMove = amount
+                        run {
+                            var y = 0
+                            while (amountLeftToMove != 0 && y < SHIP_HEIGHT) {
+                                if (cargo[y][fromX]!!.trim().isNotEmpty()) {
+                                    tempCrane.add(cargo[y][fromX])
+                                    cargo[y][fromX] = EMPTY_CONTAINER
+                                    amountLeftToMove--
+                                }
+                                y++
+                            }
+                        }
+                        //find Index where container starts (when column is empty, then SHIP_HEIGHT + 1)
+                        var highestContainerIndex = SHIP_HEIGHT
+                        var toY = 0
+                        while (highestContainerIndex == SHIP_HEIGHT && toY < SHIP_HEIGHT) {
+                            if (cargo[toY][toX]!!.trim().isNotEmpty()) {
+                                highestContainerIndex = toY
+                            }
+                            toY++
+                        }
+                        try {
+                            //start filling column to the top till no more container are left to move
+                            for (y in highestContainerIndex - 1 downTo highestContainerIndex - 1 - tempCrane.size + 1) {
+                                cargo[y][toX] = tempCrane[highestContainerIndex - 1 - y]
+                            }
+                        } catch (e: Exception) {
+                            System.err.println("amount: $amount, fromX: $fromX, toX $toX")
+                            exitProcess(4)
+                        }
+                    } else {
+                        println("line skipped: $index")
                     }
                 }
             }
-        }
-        System.out.println("All letters of highest Container of a column combined: " + stringBuilder);
-    }
-
-    private static Ship analyseShipSize(List<String> lines) {
-        Ship ship = new Ship();
-        // analyse amount of columns by finding the row with column labeling (1 2 3 ...) which only contains numbers
-        int x = 0;
-        int y = 0;
-        for (String line : lines) {
-            try {
-                // check if line only contains numbers
-                Integer.parseInt(line.replaceAll("\\s", ""));
-                // pulls all numbers out and saves the last one which should be the highest
-                x = Integer.parseInt(line.replaceAll("\\s", "").substring(line.replaceAll("\\s", "").length() - 1));
-                ship.setCargoColumns(x);
-                ship.setInitializeHighestPoint(y);
-                break;
-            } catch (Exception ignored) {
-                y++;
+            println("End State of Cargos: ")
+            for (row in cargo) println(row.contentToString())
+            println()
+            //Get Highest Container Letter
+            val stringBuilder = StringBuilder()
+            for (x in 0 until ship.cargoColumns) {
+                for (y in 0 until SHIP_HEIGHT) {
+                    if (cargo[y][x]!!.trim().isNotEmpty()) {
+                        val p = Pattern.compile("[A-Z]")
+                        val m = p.matcher(cargo[y][x]!!)
+                        if (m.find()) {
+                            stringBuilder.append(m.group())
+                            break
+                        }
+                    }
+                }
             }
+            println("All letters of highest Container of a column combined: $stringBuilder")
         }
-        ship.setCargo(new String[SHIP_HEIGHT][x]);
-        for (String[] array : ship.getCargo()) Arrays.fill(array, "    ");
-        return ship;
+
+        private fun analyseShipSize(lines: List<String>): Ship {
+            val ship = Ship()
+            // analyse amount of columns by finding the row with column labeling (1 2 3 ...) which only contains numbers
+            var x = 0
+            var y = 0
+            for (line in lines) {
+                try {
+                    // check if line only contains numbers
+                    line.replace("\\s".toRegex(), "").toInt()
+                    // pulls all numbers out and saves the last one which should be the highest
+                    val numbers = line.replace("\\s".toRegex(), "")
+                    x = numbers.substring(numbers.length - 1).toInt()
+                    ship.cargoColumns = x
+                    ship.initializeHighestPoint = y
+                    break
+                } catch (ignored: Exception) {
+                    y++
+                }
+            }
+            ship.cargo = Array(SHIP_HEIGHT) { arrayOfNulls(x) }
+            for (array in ship.cargo) Arrays.fill(array, "    ")
+            return ship
+        }
     }
 }
--- src/main/kotlin/dayFive/Ship.kt
+++ src/main/kotlin/dayFive/Ship.kt
@@ -1,34 +1,7 @@
-package dayFive;
+package dayFive
 
-public class Ship {
-    String[][] cargo;
-    int initializeHighestPoint;
-    int cargoColumns;
-
-    public Ship() {
-    }
-
-    public String[][] getCargo() {
-        return cargo;
-    }
-
-    public void setCargo(String[][] cargo) {
-        this.cargo = cargo;
-    }
-
-    public int getInitializeHighestPoint() {
-        return initializeHighestPoint;
-    }
-
-    public void setInitializeHighestPoint(int initializeHighestPoint) {
-        this.initializeHighestPoint = initializeHighestPoint;
-    }
-
-    public int getCargoColumns() {
-        return cargoColumns;
-    }
-
-    public void setCargoColumns(int cargoColumns) {
-        this.cargoColumns = cargoColumns;
-    }
+class Ship {
+    var cargo: Array<Array<String?>> = arrayOf()
+    var initializeHighestPoint = 0
+    var cargoColumns = 0
 }
