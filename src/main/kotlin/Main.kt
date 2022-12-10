47a605a9b5eb7ede9c931f8c3f0a157a144785a4
SKY-Tobi <tobias.heierli@lernende.bbw.ch>
2022-12-10 21:02:05 +0100

    User can now type in the specific day to run or "all" for all

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,71 +1,115 @@
 import FileReader.Companion.readFileAsLinesUsingReadLines
 
 fun main() {
-    var lines = readFileAsLinesUsingReadLines("src/main/resources/DayOneInput.txt")
+    print("enter day number or \"all\": ")
+    when (readLine()) {
+        "1" -> dayOne()
+        "2" -> dayTwo()
+        "3" -> dayThree()
+        "4" -> dayFour()
+        "5" -> dayFive()
+        "6" -> daySix()
+        "7" -> daySeven()
+        "8" -> dayEight()
+        "9" -> dayNine()
+        "all" -> {
+            dayOne()
+            dayTwo()
+            dayThree()
+            dayFour()
+            dayFive()
+            daySix()
+            daySeven()
+            dayEight()
+            dayNine()
+        }
+
+        else -> println("invalid input")
+    }
+}
+
+fun dayOne() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DayOneInput.txt")
     println("Day 1:")
     println("Part One:")
     dayOne.PartOne.execute(lines)
     println("Part Two:")
     dayOne.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DayTwoInput.txt")
+fun dayTwo() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DayTwoInput.txt")
     println("Day 2:")
     println("Part One:")
     dayTwo.PartOne.execute(lines)
     println("Part Two:")
     dayTwo.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DayThreeInput.txt")
+fun dayThree() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DayThreeInput.txt")
     println("Day 3:")
     println("Part One:")
     dayThree.PartOne.execute(lines)
     println("Part Two:")
     dayThree.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DayFourInput.txt")
+fun dayFour() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DayFourInput.txt")
     println("Day 4:")
     println("Part One:")
     dayFour.PartOne.execute(lines)
     println("Part Two:")
     dayFour.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DayFiveInput.txt")
+fun dayFive() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DayFiveInput.txt")
     println("Day 5:")
     println("Part One:")
     dayFive.PartOne.execute(lines)
     println("Part Two:")
     dayFive.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DaySixInput.txt")
+fun daySix() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DaySixInput.txt")
     println("Day 6:")
     println("Part One:")
     daySix.PartOne.execute(lines)
     println("Part Two:")
     daySix.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DaySevenInput.txt")
+fun daySeven() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DaySevenInput.txt")
     println("Day 7:")
     println("Part One:")
     daySeven.PartOne.execute(lines)
     println("Part Two:")
     daySeven.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DayEightInput.txt")
+fun dayEight() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DayEightInput.txt")
     println("Day 8:")
     println("Part One:")
     dayEight.PartOne.execute(lines)
     println("Part Two:")
     dayEight.PartTwo.execute(lines)
     println()
+}
 
-    lines = readFileAsLinesUsingReadLines("src/main/resources/DayNineInput.txt")
+fun dayNine() {
+    val lines = readFileAsLinesUsingReadLines("src/main/resources/DayNineInput.txt")
     println("Day 9:")
     println("Part One:")
     dayNine.PartOne.execute(lines)
