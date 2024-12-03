abc07c1680b8d48843714d200894391ba054588b
Tobias Heierli <heiertob@students.zhaw.ch>
2024-12-03 10:54:29 +0100

    Day 3 Code adjustment

--- src/main/kotlin/twentyTwentyFour/dayThree/PartTwo.kt
+++ src/main/kotlin/twentyTwentyFour/dayThree/PartTwo.kt
@@ -10,22 +10,26 @@ class PartTwo {
             val regexNumbers = Regex("""\d+""")
             var multiplicationEnabled = true
 
-            val numbers = lines.map { line ->
-                extractMatchingStrings(line, regexMul)
-            }.flatten().mapNotNull {
-                if (it == DO) {
-                    multiplicationEnabled = true
-                    null
-                } else if (it == DONT) {
-                    multiplicationEnabled = false
-                    null
-                } else if (!multiplicationEnabled) {
-                    null
-                } else {
-                    val numbers = extractMatchingStrings(it, regexNumbers)
-                    Pair(numbers[0].toInt(), numbers[1].toInt())
+            val numbers = lines.flatMap { line ->
+                extractMatchingStrings(line, regexMul).mapNotNull {
+                    when (it) {
+                        DO -> {
+                            multiplicationEnabled = true
+                            null
+                        }
+                        DONT -> {
+                            multiplicationEnabled = false
+                            null
+                        }
+                        else -> if (multiplicationEnabled) {
+                            val numbers = extractMatchingStrings(it, regexNumbers)
+                            Pair(numbers[0].toInt(), numbers[1].toInt())
+                        } else {
+                            null
+                        }
+                    }
                 }
-                }
+            }
 
             val sum = numbers.fold(0) { acc, (a, b) -> acc + a * b }
 
