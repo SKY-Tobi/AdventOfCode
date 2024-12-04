aadafee05d9ffcded8abd0799ca7e8bf9f0383bc
Tobias Heierli <heiertob@students.zhaw.ch>
2024-12-04 20:47:56 +0100

    2024 Day 4 Part One Code adjustment

--- src/main/kotlin/twentyTwentyFour/dayFour/Direction.kt
+++ src/main/kotlin/twentyTwentyFour/dayFour/Direction.kt
@@ -1,12 +1,18 @@
 package twentyTwentyFour.dayFour
 
 enum class Direction {
-    TOP_LEFT,
-    TOP,
-    TOP_RIGHT,
-    RIGHT,
-    BOTTOM_RIGHT,
-    BOTTOM,
-    BOTTOM_LEFT,
-    LEFT
+    TOP_LEFT, TOP, TOP_RIGHT, RIGHT, BOTTOM_RIGHT, BOTTOM, BOTTOM_LEFT, LEFT;
+
+    fun getDeltas(): Pair<Int, Int> {
+        return when (this) {
+            TOP_LEFT -> Pair(-1, -1)
+            TOP -> Pair(0, -1)
+            TOP_RIGHT -> Pair(1, -1)
+            RIGHT -> Pair(1, 0)
+            BOTTOM_RIGHT -> Pair(1, 1)
+            BOTTOM -> Pair(0, 1)
+            BOTTOM_LEFT -> Pair(-1, 1)
+            LEFT -> Pair(-1, 0)
+        }
+    }
 }
--- src/main/kotlin/twentyTwentyFour/dayFour/PartOne.kt
+++ src/main/kotlin/twentyTwentyFour/dayFour/PartOne.kt
@@ -12,7 +12,9 @@ class PartOne {
                 row.forEachIndexed { x, _ ->
                     if (map[y][x] == XMAS_STRING[0]) {
                         Direction.values().forEach { direction ->
-                            xmasCount += checkAroundForXMASString(map, x, y, 1, direction, 0)
+                            if(checkDirectionForXMASString(map, x, y, 1, direction)) {
+                                xmasCount++
+                            }
                         }
                     }
                 }
@@ -25,54 +27,20 @@ class PartOne {
             return input.map { it.toCharArray() }.toTypedArray()
         }
 
-        private fun checkAroundForXMASString(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int, directionToCheck: Direction, XMASCount: Int): Int {
+        private fun checkDirectionForXMASString(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int, directionToCheck: Direction): Boolean {
             if (currentCharIndex == XMAS_STRING.length) {
-                return XMASCount + 1
+                return true
             }
 
-            when(directionToCheck) {
-                Direction.TOP_LEFT -> {
-                    if (checkTopLeft(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.TOP_LEFT, XMASCount)
-                    }
-                }
-                Direction.TOP -> {
-                    if (checkTop(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.TOP, XMASCount)
-                    }
-                }
-                Direction.TOP_RIGHT -> {
-                    if (checkTopRight(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.TOP_RIGHT, XMASCount)
-                    }
-                }
-                Direction.RIGHT -> {
-                    if (checkRight(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.RIGHT, XMASCount)
-                    }
-                }
-                Direction.BOTTOM_RIGHT -> {
-                    if (checkBottomRight(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.BOTTOM_RIGHT, XMASCount)
-                    }
-                }
-                Direction.BOTTOM -> {
-                    if (checkBottom(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.BOTTOM, XMASCount)
-                    }
-                }
-                Direction.BOTTOM_LEFT -> {
-                    if (checkBottomLeft(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.BOTTOM_LEFT, XMASCount)
-                    }
-                }
-                Direction.LEFT -> {
-                    if (checkLeft(input, x, y, currentCharIndex)) {
-                        return checkAroundForXMASString(input, x, y, currentCharIndex + 1, Direction.LEFT, XMASCount)
-                    }
-                }
+            val (dx, dy) = directionToCheck.getDeltas()
+            val newX = x + dx * currentCharIndex
+            val newY = y + dy * currentCharIndex
+
+            return if (checkCharacter(input, newX, newY, currentCharIndex)) {
+                checkDirectionForXMASString(input, x, y, currentCharIndex + 1, directionToCheck)
+            } else {
+                false
             }
-            return XMASCount
         }
 
         private fun checkCharacter(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
@@ -80,38 +48,6 @@ class PartOne {
                 return false
             }
             return input[y][x] == XMAS_STRING[currentCharIndex]
-        }
-
-        private fun checkTopLeft(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x - currentCharIndex, y - currentCharIndex, currentCharIndex)
-        }
-
-        private fun checkTop(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x, y - currentCharIndex, currentCharIndex)
-        }
-
-        private fun checkTopRight(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x + currentCharIndex, y - currentCharIndex, currentCharIndex)
-        }
-
-        private fun checkRight(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x + currentCharIndex, y, currentCharIndex)
-        }
-
-        private fun checkBottomRight(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x + currentCharIndex, y + currentCharIndex, currentCharIndex)
-        }
-
-        private fun checkBottom(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x, y + currentCharIndex, currentCharIndex)
-        }
-
-        private fun checkBottomLeft(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x - currentCharIndex, y + currentCharIndex, currentCharIndex)
-        }
-
-        private fun checkLeft(input: Array<CharArray>, x: Int, y: Int, currentCharIndex: Int): Boolean {
-            return checkCharacter(input, x - currentCharIndex, y, currentCharIndex)
         }
     }
 }
