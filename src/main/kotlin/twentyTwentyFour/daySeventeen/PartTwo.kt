e87b65a925520835a1599829486d122fcc4ff973
Tobias Heierli <heiertob@students.zhaw.ch>
2024-12-17 20:59:52 +0100

    2024 Day 17 Part 2 Done WTF

--- src/main/kotlin/twentyTwentyFour/daySeventeen/PartTwo.kt
+++ src/main/kotlin/twentyTwentyFour/daySeventeen/PartTwo.kt
@@ -1,67 +1,69 @@
 package twentyTwentyFour.daySeventeen
 
+import kotlin.math.pow
+
 class PartTwo {
     companion object {
         fun execute(lines: List<String>) {
             val parts = lines.joinToString("\n").split("\n\n")
-            val initialRegisters = Regex("\\d+").findAll(parts[0]).map { it.value.toInt() }.toList()
-            val program = Regex("\\d+").findAll(parts[1]).map { it.value.toInt() }.toList()
-            val (initialRegisterB, initialRegisterC) = initialRegisters.drop(1)
-            var correctRegisterA = 1
+            val program = Regex("\\d+").findAll(parts[1]).map { it.value.toLong() }.toList()
 
-            while (true) {
-                var (registerA, registerB, registerC) = listOf(correctRegisterA, initialRegisterB, initialRegisterC)
-                var instructionPointer = 0
-                val output = mutableListOf<Int>()
-
-                while (instructionPointer < program.size) {
-                    val opcode = program[instructionPointer]
-                    val operand = program.getOrElse(instructionPointer + 1) { 7 }
-                    instructionPointer += if (opcode == 3 && registerA != 0) 0 else 2
-
-                    when (opcode) {
-                        0 -> registerA /= calculateDenominator(operand, registerA, registerB, registerC)
-                        1 -> registerB = registerB xor operand
-                        2 -> registerB = getRegisterValue(operand, registerA, registerB, registerC) % 8
-                        3 -> if (registerA != 0) instructionPointer = operand
-                        4 -> registerB = registerB xor registerC
-                        5 -> output.add(getRegisterValue(operand, registerA, registerB, registerC) % 8)
-                        6 -> registerB = registerA / calculateDenominator(operand, registerA, registerB, registerC)
-                        7 -> registerC = registerA / calculateDenominator(operand, registerA, registerB, registerC)
-                        else -> throw IllegalArgumentException("Invalid opcode")
-                    }
-
-                    if(output != program.dropLast(program.size - output.size)) {
-                        break
-                    }
-
-                }
-
-                if (output == program) {
-                    println("Lowest positive initial value for register A: $correctRegisterA")
-                    return
-                }
-
-                correctRegisterA++
-            }
+            println(findAMatchingOutput(program, program))
         }
 
-        private fun calculateDenominator(operand: Int, registerA: Int, registerB: Int, registerC: Int): Int {
-            return 1 shl when (operand) {
-                in 0..3 -> operand
-                4 -> registerA
-                5 -> registerB
-                6 -> registerC
-                else -> throw IllegalArgumentException("Invalid operand")
+        private fun compute(program: List<Long>, aStart: Long): MutableList<Long> {
+            var registerA = aStart
+            var registerB = 0L
+            var registerC = 0L
+            var instructionPointer = 0
+            val output = mutableListOf<Long>()
+
+            while (instructionPointer < program.size) {
+                val opcode = program[instructionPointer]
+                val operand = program.getOrElse(instructionPointer + 1) { 7 }
+                instructionPointer += if (opcode == 3L && registerA != 0L) 0 else 2
+
+                when (opcode) {
+                    0L -> registerA /= calculateDenominator(operand, registerA, registerB, registerC)
+                    1L -> registerB = registerB xor operand
+                    2L -> registerB = getRegisterValue(operand, registerA, registerB, registerC) % 8
+                    3L -> if (registerA != 0L) instructionPointer = operand.toInt()
+                    4L -> registerB = registerB xor registerC
+                    5L -> output.add(getRegisterValue(operand, registerA, registerB, registerC) % 8)
+                    6L -> registerB = registerA / calculateDenominator(operand, registerA, registerB, registerC)
+                    7L -> registerC = registerA / calculateDenominator(operand, registerA, registerB, registerC)
+                    else -> throw IllegalArgumentException("Invalid opcode")
+                }
             }
+
+            return output
         }
 
-        private fun getRegisterValue(operand: Int, registerA: Int, registerB: Int, registerC: Int): Int {
+        // https://www.reddit.com/r/adventofcode/comments/1hg38ah/comment/m2gfogr/
+        private fun findAMatchingOutput(program: List<Long>, targetOutput: List<Long>): Long {
+            var registerA = if (targetOutput.size == 1) {
+                0
+            } else {
+                8 * findAMatchingOutput(program, targetOutput.subList(1, targetOutput.size))
+            }
+
+            while(compute(program, registerA) != targetOutput) {
+                registerA++
+            }
+
+            return registerA
+        }
+
+        private fun calculateDenominator(operand: Long, registerA: Long, registerB: Long, registerC: Long): Long {
+            return 2.0.pow(getRegisterValue(operand, registerA, registerB, registerC).toDouble()).toLong()
+        }
+
+        private fun getRegisterValue(operand: Long, registerA: Long, registerB: Long, registerC: Long): Long {
             return when (operand) {
                 in 0..3 -> operand
-                4 -> registerA
-                5 -> registerB
-                6 -> registerC
+                4L -> registerA
+                5L -> registerB
+                6L -> registerC
                 else -> throw IllegalArgumentException("Invalid operand")
             }
         }
