8ce0d4efb85af68bd92b982fdc9a5250cec354fe
Tobias Heierli <heiertob@students.zhaw.ch>
2024-12-22 23:49:12 +0100

    2024 Day 20 Part 1 Done

--- src/main/kotlin/twentyTwentyFour/dayTwenty/PartTwo.kt
+++ src/main/kotlin/twentyTwentyFour/dayTwenty/PartTwo.kt
@@ -1,9 +1,117 @@
 package twentyTwentyFour.dayTwenty
 
+import java.util.*
+import java.util.concurrent.atomic.AtomicInteger
+
 class PartTwo {
     companion object {
         fun execute(lines: List<String>) {
+            val map = lines.map { it.toCharArray() }
+            val start = findPoint(map, 'S')
+            val end = findPoint(map, 'E')
 
+            println("NOT WORKING, DO NOT USE")
+
+            /*val minStepsWithoutCheat = aStar(map, start, end, Pair(false, AtomicInteger(0))).first
+            val allCheatPaths = findAllCheatPaths(map, start, end)
+                .filter { it < minStepsWithoutCheat }
+                .sortedBy { minStepsWithoutCheat - it }
+
+            val cheatGroups = allCheatPaths.groupingBy { it }.eachCount()
+            cheatGroups.forEach { (timeSaved, count) ->
+                println("There ${if (count > 1) "are" else "is"} $count cheat${if (count > 1) "s" else ""} that save${if (count > 1) "" else "s"} ${minStepsWithoutCheat - timeSaved} picosecond${if (timeSaved > 1) "s" else ""}.")
+            }
+
+            println("There are ${cheatGroups.filter { minStepsWithoutCheat - it.key >= 100 }.values.sum()} cheats that save you at least 100 picoseconds.")*/
+        }
+
+        private fun findPoint(map: List<CharArray>, char: Char): Pair<Int, Int> {
+            for (y in map.indices) {
+                for (x in map[y].indices) {
+                    if (map[y][x] == char) {
+                        return Pair(x, y)
+                    }
+                }
+            }
+            throw IllegalArgumentException("Point $char not found in the map")
+        }
+
+        private fun aStar(
+            map: List<CharArray>,
+            start: Pair<Int, Int>,
+            end: Pair<Int, Int>,
+            cheat: Pair<Boolean, AtomicInteger>
+        ): Pair<Int, Pair<Int, Int>?> {
+            val openSet = PriorityQueue(compareBy<State> { it.steps + heuristic(it.point, end) })
+            val gScore = mutableMapOf<Pair<Int, Int>, Int>().withDefault { Int.MAX_VALUE }
+            gScore[start] = 0
+            openSet.add(State(start, 0, false))
+
+            while (openSet.isNotEmpty()) {
+                val current = openSet.poll()
+                if (current.point == end) {
+                    return Pair(current.steps, current.point)
+                }
+
+                val canCheat = cheat.first && cheat.second.decrementAndGet() >= 0
+                if(cheat.first && !canCheat) {
+                    return Pair(current.steps, current.point)
+                }
+
+                for (direction in Direction.values()) {
+                    val (dx, dy) = direction.getDeltas()
+                    val nextPoint = Pair(current.point.first + dx, current.point.second + dy)
+                    if (isValid(map, nextPoint) || canCheat) {
+                        val tentativeGScore = gScore.getValue(current.point) + 1
+                        if (tentativeGScore < gScore.getValue(nextPoint)) {
+                            gScore[nextPoint] = tentativeGScore
+                            openSet.add(State(nextPoint, tentativeGScore, current.cheated))
+                        }
+                    }
+                }
+            }
+            return Pair(Int.MAX_VALUE, null)
+        }
+
+        private fun heuristic(point: Pair<Int, Int>, end: Pair<Int, Int>): Int {
+            return Math.abs(point.first - end.first) + Math.abs(point.second - end.second)
+        }
+
+        private fun findAllCheatPaths(map: List<CharArray>, start: Pair<Int, Int>, end: Pair<Int, Int>): List<Int> {
+            val queue: Queue<State> = LinkedList()
+            val visited = mutableMapOf<Pair<Int, Int>, Int>()
+            val breachedWallsWithCheat = mutableSetOf<Pair<Pair<Int, Int>, Pair<Int, Int>>>()
+            val cheatPaths = mutableListOf<Int>()
+            queue.add(State(start, 0, false))
+
+            while (queue.isNotEmpty()) {
+                val current = queue.poll()
+
+                for (direction in Direction.values()) {
+                    val (dx, dy) = direction.getDeltas()
+                    val nextPoint = Pair(current.point.first + dx, current.point.second + dy)
+                    if (isValid(map, nextPoint)) {
+                        val nextState = State(nextPoint, current.steps + 1, false)
+                        if (nextState.steps < visited.getOrDefault(nextState.point, Int.MAX_VALUE)) {
+                            visited[nextState.point] = nextState.steps
+                            queue.add(nextState)
+                        }
+                    } else if (!current.cheated) {
+                        repeat(20) {
+                            val cheatSteps = aStar(map, current.point, end, Pair(true, AtomicInteger(it+1)))
+                            if (cheatSteps.first != Int.MAX_VALUE) {
+                                cheatPaths.add(cheatSteps.first + current.steps + 2)
+                                breachedWallsWithCheat.add(Pair(nextPoint, cheatSteps.second!!))
+                            }
+                        }
+                    }
+                }
+            }
+            return cheatPaths
+        }
+
+        private fun isValid(map: List<CharArray>, point: Pair<Int, Int>): Boolean {
+            return point.second in map.indices && point.first in map[point.second].indices && map[point.second][point.first] != '#'
         }
     }
 }
---
+++ src/main/kotlin/twentyTwentyFour/dayTwenty/State.kt
@@ -0,0 +1,3 @@
+package twentyTwentyFour.dayTwenty
+
+data class State(val point: Pair<Int, Int>, val steps: Int, val cheated: Boolean)
